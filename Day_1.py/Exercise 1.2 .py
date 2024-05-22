3eb05d13da3414d85f822f2c1b1f7090bd3fbb3b
Yemight25 <adeyemisodik670@gmail.com>
2024-05-22 21:39:04 +0100

    Debugging codes

---
+++ Day_1.py/Exercise 1.2 .py
@@ -0,0 +1,6 @@
+# debugging codes
+print("Day-1 String Manipulation")
+print('String concatenation is with the " + " sign')
+print('e.g. print( "Hello " + "World" )')
+print("New lines can be created with a backslash and n")
+
