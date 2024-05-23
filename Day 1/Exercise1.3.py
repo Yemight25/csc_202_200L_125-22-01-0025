0b5c28ac583ae5c4cf52161e82facfc260e89a6c
Yemight25 <adeyemisodik670@gmail.com>
2024-05-23 10:27:56 +0100

    using input function and calculate the length of the string

--- Day 1/Exercise1.3.py
+++ Day 1/Exercise1.3.py
@@ -1,5 +1,9 @@
-# write a programe that print the number of characters in a user's name and calculate the length of a string.
+# write a programe that print the number of 
+# characters in a user's name and calculate the length of a string.
+
 print(len(input("what is your name? ")))
+
 # Another menthod to used
+
 name = input("what is your favourite fruit" )
 print(len(name))
