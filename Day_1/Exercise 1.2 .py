907c72376040be54f1ed123fc0e661f41329534d
Yemight25 <adeyemisodik670@gmail.com>
2024-05-23 09:09:24 +0100

    Delete Day_1 directory

--- Day_1/Exercise 1.2 .py
+++
@@ -1,6 +0,0 @@
-# debugging codes
-print("Day-1 String Manipulation")
-print('String concatenation is with the " + " sign')
-print('e.g. print( "Hello " + "World" )')
-print("New lines can be created with a backslash and n")
-
--- Day_1/Exercise1.4.py
+++
@@ -1,8 +0,0 @@
-# Write a program that switches the value stored in the variable a and b
-a= input("Enter the value of a? ")
-b= input(" Enter the value of b?")
-print("a=" ,b )
-print("b=", a)
-# another method 
-print( f'a= {b}\nb= {a}')
-
