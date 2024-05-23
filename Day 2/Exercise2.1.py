3111baed20ea66b4258ee682abb20c76d9cc66e2
Yemight25 <adeyemisodik670@gmail.com>
2024-05-23 10:37:36 +0100

    add two digits number from a single input

--- Day 2/Exercise2.1.py
+++ Day 2/Exercise2.1.py
@@ -1,6 +1,14 @@
 digit = input("Enter the two digits number? ")
 first = int(digit[0])
 second = int(digit[1])
+
+#Method for splitting the input values
+print(first)
+print(second)
+result = first + second
+print(result)
+
+#Alternative method
 print(f"{first}\n{second}")
 result = first + second
 print(result)
