9a6a8732f86375310f9e7827cbdf18dd8775a153
Yemight25 <adeyemisodik670@gmail.com>
2024-05-29 00:59:24 +0100

    Exercise on if, elif, and else  statement

---
+++ Day 3/Exercise3.2.py
@@ -0,0 +1,14 @@
+print(" welcome to the rollercoaster! ")
+height = int(input(" What is your height in cm? "))
+
+if height >= 120: 
+    print(" You can ride the rollercoaster!")
+    age = int(input("What ia your age? "))
+    if age < 12:
+      print(" please pay $5.")
+    elif age<=18:
+     print("please pay $7.")
+    else:
+      print( 'please pay $12.')
+else:
+  print(" sorry, have to grow taller before you can ride.")
