0a4e338edcbb84d5272139a3a6892ad6eb1cc892
Yemight25 <adeyemisodik670@gmail.com>
2024-06-15 01:49:20 +0100

    Day_9 Exercise and Project

---
+++ Day 9/Exercise 1.py
@@ -0,0 +1,16 @@
+student_scores = {"Harry": 81, "Ron": 78, "Hermione": 99, "Draco": 74, "Neville": 62}
+
+student_grade = {}
+
+for key in student_scores:
+    if student_scores[key] > 90:
+        student_grade[key] = "Outstanding"
+    elif student_scores[key] > 80:
+        student_grade[key] = "Exceeds Expectation"
+    elif student_scores[key] > 70:
+        student_grade[key] = "Acceptable"
+    else:
+        student_grade[key] = "Fail"
+
+print(student_scores)
+print(student_grade)
